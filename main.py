192eba171f5a244d12e2c2295d14ed2f610de8de
Oskar Baginski <oskbagin@gmail.com>
2022-11-23 22:41:19 +0100

    first draft of the class

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import serial
 import serial.tools.list_ports
+import threading
+import sys
 
 
 class PySerial:
-    def __init__(self, port=None):
+    def inputThread(self):
+        print("Input thread")
+        while True:
+            command = input()
+            if command.lower() == "exit":
+                self.kill_session = True
+                break
+
+            command = bytes(command, encoding="utf-8")
+            print(f"Sending {command} command")
+        print("Killing input thread")
+
+
+    def serialReceiveThread(self):
+        print("uart thread")
+        # CMD input thread will close the program
+        while not self.kill_session:
+            data = self.device.readline()
+            #print(data)
+        print("Killing serial rx thread")
+
+    def __init__(self, port=None, baudrate=115200):
+        self.kill_session = False
         ports = serial.tools.list_ports.comports()
         devices = []
         i = 0
@@ -18,11 +42,23 @@ class PySerial:
             else:
                 port = 0
 
-        self.device = serial.Serial(devices[int(port)][0])
+        self.device = serial.Serial(devices[int(port)][0], baudrate)
 
     def __del__(self):
         # Close the connection once port is unused
         self.device.close()
 
+    def loop(self):
+        self.input_thread = threading.Thread(target=self.inputThread, daemon=True)
+        self.serial_rx_thread = threading.Thread(target=self.serialReceiveThread, daemon=True)
+
+        self.input_thread.start()
+        self.serial_rx_thread.start()
+
+        self.input_thread.join()
+        self.serial_rx_thread.join()
+
 
 s = PySerial()
+s.loop()
+
